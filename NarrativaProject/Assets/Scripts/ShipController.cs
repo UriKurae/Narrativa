d9a144af0a550d394a6ce56a8bd3d8c4421f1752
Adrian Aroca Mascaraque <61042878+adrianam4@users.noreply.github.com>
2023-04-04 16:46:19 +0200

    BARKO

--- NarrativaProject/Assets/Scripts/ShipController.cs
+++ NarrativaProject/Assets/Scripts/ShipController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ShipController : MonoBehaviour
 {
@@ -10,13 +11,18 @@ public class ShipController : MonoBehaviour
         BATTLE
     }
 
+    public List<GameObject> points;
     public Stance currentStance;
     public GameObject target;
     public float velocity = 5.0f;
+    private bool hasArrived = false;
+    private int index = 0;
+    private int totalPoints = 0;
     // Start is called before the first frame update
     void Start()
     {
-        currentStance = Stance.BATTLE;
+        currentStance = Stance.ROAMING;
+        totalPoints = points.Count - 1;
     }
 
     // Update is called once per frame
@@ -24,5 +30,18 @@ public class ShipController : MonoBehaviour
     {
         if (currentStance == Stance.BATTLE)
             transform.RotateAround(target.transform.position, Vector3.up, -velocity * Time.deltaTime);
+        else
+        {
+            var step = velocity * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, points[index].gameObject.transform.position, step);
+            Vector3 direction = (points[index + 1].transform.position - points[index].transform.position).normalized;
+            this.gameObject.transform.rotation = Quaternion.Euler(direction);
+
+            if (transform.position == points[index].gameObject.transform.position)
+            {
+                ++index;
+                index = index % totalPoints;
+            }
+        }
     }
 }
