632839301575ef202074e2d0313254461e60d52c
Oriol Bernal Martinez <youtfrost@gmail.com>
2023-04-09 23:12:19 +0200

    Change of scenes, and dialogues

--- NarrativaProject/Assets/Scripts/TransitionManager.cs
+++ NarrativaProject/Assets/Scripts/TransitionManager.cs
@@ -8,10 +8,19 @@ public class TransitionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.F1))
         {
             ChangeScene("InteriorScene");
         }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            ChangeScene("BeachScene");
+        }
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            ChangeScene("ShipScene");
+        }
+
     }
 
     public void ChangeScene(string scene)
