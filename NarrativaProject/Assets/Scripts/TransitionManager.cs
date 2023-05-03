f82017024eb15b858bac5a73758e58f66f2126fa
Adrian Aroca Mascaraque <61042878+adrianam4@users.noreply.github.com>
2023-05-03 17:29:06 +0200

    load screen

--- NarrativaProject/Assets/Scripts/TransitionManager.cs
+++ NarrativaProject/Assets/Scripts/TransitionManager.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class TransitionManager : MonoBehaviour
 {
+    public TextMeshProUGUI progressText;
+    public Slider progressSlider;
+
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.L))
+            StartCoroutine(LoadNewScene());
+
         if (Input.GetKeyDown(KeyCode.F1))
         {
             ChangeScene("InteriorScene");
@@ -26,5 +34,21 @@ public class TransitionManager : MonoBehaviour
     public void ChangeScene(string scene)
     {
         SceneManager.LoadScene(scene);
+    }
+
+    private IEnumerator LoadNewScene()
+    {
+        progressSlider.gameObject.SetActive(true);
+        AsyncOperation operationLoad = SceneManager.LoadSceneAsync(1);
+
+        while (operationLoad.isDone != true)
+        {
+            float progress = Mathf.Clamp01(operationLoad.progress / .00f);
+            progressSlider.value = progress;
+            progressText.text = (progress * 100) + "%";
+
+            yield return null;
+        }
+
     }
 }
